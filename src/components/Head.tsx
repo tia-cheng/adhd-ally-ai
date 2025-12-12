53c630ac31668acc44d7fa95dfd4820de621d6af
Tia <tianxcheng888@gmail.com>
2025-12-12 00:02:27 -0800

    finish HomePage responsive

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -15,3 +15,19 @@ const Head: React.FC<HeadProps>  = ({title}) => {
 }
 
 export default Head
+
+//type: This is a TypeScript keyword. It means "I am about to define a custom shape for data."
+//HeadProps: This is just a name you gave it. It could be HeadRules or PropsForHead. Standard naming convention is [ComponentName]Props.
+//{ title: string; }: This is the rule itself.
+//Key (title): The input name must be exactly "title".
+//Value (string): The data passed in must be text (like "Home")
+
+//: React.FC
+//This stands for React Functional Component.
+//It is a built-in TypeScript type provided by React.
+//It tells the compiler: "This variable isn't just a number or a string. It is a specific kind of function that returns HTML/JSX."
+
+//= ({ title }) => { ... }
+//This is the function itself.
+//Because you defined <HeadProps> earlier, TypeScript knows that the first argument here must have a property called title.
+//{ title }: This extracts the title text directly so you can use it inside the function.
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,23 +5,43 @@ import { Link } from 'react-router';
 import { Button } from '@/components/ui/button';
 import Head from '@/components/Head';
 
+/* Assets */
+import { heroBannerSm, heroBannerLg} from '@/assets'
+
 
 const HomePage = () => {
   return (
     <>
       <Head title='ADHD Ally AI - AI-Powered To-Do List and Project Management App'/>
+      
       <section>
-        <div className=''>
-          <div className=''>
-            <h1 className=''>
-              Simplify Your Work and Life with AI-Powered Task Management
+        <div className='container !px-8 grid grid-cols-1 gap-6 items-center xl:gap-12 xl:grid-cols-[1fr_1.5fr]'>
+          <div className='flex flex-col items-center text-center space-y-2 lg:text-left lg:items-left lg:space-y-6'>
+            <h1 className='text-4xl font-semibold max-w-[22ch] md:text-5xl lg:text-6xl xl:text-5xl'>
+              Simplify Your Work and Life with{' '}
+              <span className='inline-flex bg-gradient-to-t from-primary/30 to-primary/10 rounded-2xl px-2'>AI-Powered</span> Task Management
             </h1>
+
+            <p className='max-w-[48ch] text-foreground/90 md:text-lg lg:text-xl'>
+              Simplify life for both you and your team with the world's #1 task
+              manager and to-do list app.
+            </p>
+
+            <Button asChild size='lg'>
+              <Link to='/register'>Start for FREE</Link>
+            </Button>
           </div>
+
+          <figure className='bg-secondary rounded-2xl overflow-hidden aspect-square max-md:max-w-[480px] max-md:mx-auto md:aspect-video'>
+            <img src={heroBannerSm} width={480} height={480} alt='ADHD Ally AI website' className='md:hidden'/>
+            <img src={heroBannerLg} width={960} height={540} alt='ADHD Ally AI website' className='max-md:hidden'/>
+            {/* max-md:hidden is a responsive design utility in Tailwind CSS. It translates to: "Hide this element on screens smaller than the 'Medium' breakpoint (tablets and mobile). */}
+          </figure>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
 export default HomePage
 
@@ -30,3 +50,12 @@ export default HomePage
 // HTML
 // <header>...</header>
 // <main>...</main>
+
+//<span>: Think of it like a Highlighter Pen. It changes the look of specific words, but doesn't move them to a new line.
+
+//max-w-[12ch]: 12 characters
+//!px-8 important, force
+
+//grid-cols-[1fr_1.5fr] (The Ratio)
+//Left Column (1fr): Takes up 1 part of the space.
+//Right Column (1.5fr): Takes up 1.5 parts of the space.
